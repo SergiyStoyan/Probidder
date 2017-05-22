6a9ea9553bff2a34066336d6942fdb918955d9b7
cliversoft <sergey.stoyan@gmail.com>
2017-05-22 14:25:36 +0300

    .

--- AuctionWindow.xaml.cs
+++ AuctionWindow.xaml.cs
@@ -132,17 +132,28 @@ namespace Cliver.Foreclosures
 
         private void Save_Click(object sender, RoutedEventArgs e)
         {
-            //if (string.IsNullOrWhiteSpace(subject.Text))
-            //    throw new Exception("Subject is empty.");
-            //if (string.IsNullOrWhiteSpace(this.description.Text))
-            //    throw new Exception("Description is empty 
-            Db.Foreclosure f = (Db.Foreclosure)fields.DataContext;
-            foreclosures.Save(f);
-            
-            fields.DataContext = new Db.Foreclosure();
-            Save.IsEnabled = false;
-            Delete.IsEnabled = false;
-            //Close();
+            try
+            {
+                //if (string.IsNullOrWhiteSpace(subject.Text))
+                //    throw new Exception("Subject is empty.");
+                //if (string.IsNullOrWhiteSpace(this.description.Text))
+                //    throw new Exception("Description is empty 
+
+                if (!this.IsValid())
+                    throw new Exception("Some values are incorrect. Please correct fields surrounded with red borders before saving.");
+
+                    Db.Foreclosure f = (Db.Foreclosure)fields.DataContext;
+                foreclosures.Save(f);
+
+                fields.DataContext = new Db.Foreclosure();
+                Save.IsEnabled = false;
+                Delete.IsEnabled = false;
+                //Close();
+            }
+            catch(Exception ex)
+            {
+                Message.Error2(ex);
+            }
         }
 
         private void City_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -266,6 +277,30 @@ namespace Cliver.Foreclosures
             //    Console.Beep(5000, 200);
             //    e.Handled = true;
             //}
+        }
+    }
+
+    public class TextInputToVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            // Always test MultiValueConverter inputs for non-null
+            // (to avoid crash bugs for views in the designer)
+            if (values[0] is bool && values[1] is bool)
+            {
+                bool hasText = !(bool)values[0];
+                bool hasFocus = (bool)values[1];
+
+                if (hasFocus || hasText)
+                    return Visibility.Collapsed;
+            }
+
+            return Visibility.Visible;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 
