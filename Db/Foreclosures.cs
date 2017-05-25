d381b3a6669629fbdfd9ff071102db2d2410df59
cliversoft <sergey.stoyan@gmail.com>
2017-05-26 02:57:56 +0300

    .

--- Db/Foreclosures.cs
+++ Db/Foreclosures.cs
@@ -45,7 +45,7 @@ namespace Cliver.Foreclosures
             public string SALE_LOC { get; set; }
             public DateTime? ENTRY_DATE { get; set; }
             public string LENDOR { get; set; }
-            public string ORIGINAL_MTG { get; set; }
+            public DateTime? ORIGINAL_MTG { get; set; }
             public string DOCUMENT_N { get; set; }
             public int? ORIGINAL_I { get; set; }
             public string LEGAL_D { get; set; }
@@ -110,20 +110,20 @@ namespace Cliver.Foreclosures
                 switch (propertyName)
                 {
                     case "TYPE_OF_EN":
-                        if (TYPE_OF_EN == null)
-                            return null;
-                        if (TYPE_OF_EN.Length < 1)
-                            return "Error";
+                        //if (TYPE_OF_EN == null)
+                        //    return null;
+                        //if (TYPE_OF_EN.Length < 1)
+                        //    return "Error";
                         return null;
                     case "COUNTY":
-                        if (COUNTY == null)
-                            return null;
+                        //if (COUNTY == null)
+                        //    return null;
                         if (COUNTY.Length < 1)
                             return "Error";
                         return null;
                     case "CASE_N":
-                        if (CASE_N == null)
-                            return null;
+                        //if (CASE_N == null)
+                        //    return null;
                         if (CASE_N.Length < 1)
                             return "Error";
                         return null;
@@ -140,28 +140,30 @@ namespace Cliver.Foreclosures
                             return "Error";
                         return null;
                     case "LENDOR":
-                        if (LENDOR == null)
-                            return null;
-                        if (LENDOR.Length < 1)
-                            return "Error";
+                        //if (LENDOR == null)
+                        //    return null;
+                        //if (LENDOR.Length < 1)
+                        //    return "Error";
                         return null;
                     case "ORIGINAL_MTG":
+                        //if (ORIGINAL_MTG == null)
+                        //    return "Error";
                         return null;
                     case "DOCUMENT_N":
                         return null;
                     case "ORIGINAL_I":
                         return null;
                     case "LEGAL_D":
-                        if (LEGAL_D == null)
-                            return null;
-                        if (LEGAL_D.Length < 1)
-                            return "Error";
+                        //if (LEGAL_D == null)
+                        //    return null;
+                        //if (LEGAL_D.Length < 1)
+                        //    return "Error";
                         return null;
                     case "ADDRESS":
-                        if (ADDRESS == null)
-                            return null;
-                        if (ADDRESS.Length < 1)
-                            return "Error";
+                        //if (ADDRESS == null)
+                        //    return null;
+                        //if (ADDRESS.Length < 1)
+                        //    return "Error";
                         return null;
                     case "CITY":
                         if (CITY == null)
@@ -182,76 +184,80 @@ namespace Cliver.Foreclosures
                             return "Error";
                         return null;
                     case "DATE_OF_CA":
+                        //if (DATE_OF_CA == null)
+                        //    return "Error";
                         return null;
                     case "LAST_PAY_DATE":
+                        //if (LAST_PAY_DATE == null)
+                        //    return "Error";
                         return null;
                     case "BALANCE_DU":
                         return null;
                     case "PER_DIEM_I":
                         return null;
                     case "CURRENT_OW":
-                        if (CURRENT_OW == null)
-                            return null;
-                        if (CURRENT_OW.Length < 1)
-                            return "Error";
+                        //if (CURRENT_OW == null)
+                        //    return null;
+                        //if (CURRENT_OW.Length < 1)
+                        //    return "Error";
                         return null;
                     case "IS_ORG":
                         return null;
                     case "DECEASED":
                         return null;
                     case "OWNER_ROLE":
-                        if (OWNER_ROLE == null)
-                            return null;
-                        if (OWNER_ROLE.Length < 1)
-                            return "Error";
+                        //if (OWNER_ROLE == null)
+                        //    return null;
+                        //if (OWNER_ROLE.Length < 1)
+                        //    return "Error";
                         return null;
                     case "OTHER_LIENS":
-                        if (OTHER_LIENS == null)
-                            return null;
-                        if (OTHER_LIENS.Length < 1)
-                            return "Error";
+                        //if (OTHER_LIENS == null)
+                        //    return null;
+                        //if (OTHER_LIENS.Length < 1)
+                        //    return "Error";
                         return null;
                     case "ADDL_DEF":
-                        if (ADDL_DEF == null)
-                            return null;
-                        if (ADDL_DEF.Length < 1)
-                            return "Error";
+                        //if (ADDL_DEF == null)
+                        //    return null;
+                        //if (ADDL_DEF.Length < 1)
+                        //    return "Error";
                         return null;
                     case "PUB_COMMENTS":
-                        if (PUB_COMMENTS == null)
-                            return null;
-                        if (PUB_COMMENTS.Length < 1)
-                            return "Error";
+                        //if (PUB_COMMENTS == null)
+                        //    return null;
+                        //if (PUB_COMMENTS.Length < 1)
+                        //    return "Error";
                         return null;
                     case "INT_COMMENTS":
-                        if (INT_COMMENTS == null)
-                            return null;
-                        if (INT_COMMENTS.Length < 1)
-                            return "Error";
+                        //if (INT_COMMENTS == null)
+                        //    return null;
+                        //if (INT_COMMENTS.Length < 1)
+                        //    return "Error";
                         return null;
                     case "ATTY":
-                        if (ATTY == null)
-                            return null;
-                        if (ATTY.Length < 1)
-                            return "Error";
+                        //if (ATTY == null)
+                        //    return null;
+                        //if (ATTY.Length < 1)
+                        //    return "Error";
                         return null;
                     case "ATTORNEY_S":
-                        if (ATTORNEY_S == null)
-                            return null;
-                        if (Regex.IsMatch(ATTORNEY_S, "_"))
-                            return "Error";
+                        //if (ATTORNEY_S == null)
+                        //    return null;
+                        //if (Regex.IsMatch(ATTORNEY_S, "_"))
+                        //    return "Error";
                         return null;
                     case "TYPE_OF_MO":
-                        if (TYPE_OF_MO == null)
-                            return null;
-                        if (TYPE_OF_MO.Length < 1)
-                            return "Error";
+                        //if (TYPE_OF_MO == null)
+                        //    return null;
+                        //if (TYPE_OF_MO.Length < 1)
+                        //    return "Error";
                         return null;
                     case "PROP_DESC":
-                        if (PROP_DESC == null)
-                            return null;
-                        if (PROP_DESC.Length < 1)
-                            return "Error";
+                        //if (PROP_DESC == null)
+                        //    return null;
+                        //if (PROP_DESC.Length < 1)
+                        //    return "Error";
                         return null;
                     case "INTEREST_R":
                         return null;
@@ -260,16 +266,16 @@ namespace Cliver.Foreclosures
                     case "TERM_OF_MTG":
                         return null;
                     case "DEF_ADDRESS":
-                        if (DEF_ADDRESS == null)
-                            return null;
-                        if (DEF_ADDRESS.Length < 1)
-                            return "Error";
+                        //if (DEF_ADDRESS == null)
+                        //    return null;
+                        //if (DEF_ADDRESS.Length < 1)
+                        //    return "Error";
                         return null;
                     case "DEF_PHONE":
-                        if (DEF_PHONE == null)
-                            return null;
-                        if (Regex.IsMatch(DEF_PHONE, "_"))
-                            return "Error";
+                        //if (DEF_PHONE == null)
+                        //    return null;
+                        //if (Regex.IsMatch(DEF_PHONE, "_"))
+                        //    return "Error";
                         return null;
                     default:
                         throw new Exception("Field " + propertyName + " is absent in validation.");
